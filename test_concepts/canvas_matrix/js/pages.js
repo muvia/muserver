d25ef88249f4068d694d801a0a937f1dae4e3195
Cesar Pachon <cesarpachon@gmail.com>
2014-01-21 17:23:49 -0500

    split functionality of canvas test in many files

---
+++ test_concepts/canvas_matrix/js/pages.js
@@ -0,0 +1,23 @@
+var g_current_user = null;
+var g_canvas = null;
+var g_ctx = null;
+
+//--- welcome (index) page
+$( "div#welcome-page" ).on( "pagecreate", function( event, ui ) {
+	console.log("welcome page loaded");
+	mp_add_footer(this);
+	
+	g_canvas = $("canvas#c").get(0);
+	console.log(g_canvas);
+	g_ctx = g_canvas.getContext('2d');
+
+});
+
+
+
+//---menu  page
+$( "div#menu-page" ).on( "pageinit", function( event, ui ) {
+	console.log("menu page loaded");
+} );
+
+
