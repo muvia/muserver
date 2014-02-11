5cfcd66ed135477d30197bea18e4a837ca23ce44
Cesar Pachon <cesarpachon@gmail.com>
2014-02-11 17:36:17 -0500

    add spec

---
+++ engine/specs/transform_spec.js
@@ -0,0 +1,9 @@
+/*
+ * test transform class. 
+ */
+describe("Transform ", function() {
+		it("identity transform by point keeps the point", function() {
+			
+			expect(true).toBe(true);
+ 	});
+});
