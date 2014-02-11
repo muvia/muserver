8adc578b5c673060e7212852a3bd6ae85bb5736c
Cesar Pachon <cesarpachon@gmail.com>
2014-02-11 16:08:23 -0500

    changes to gruntfile.js

--- engine/Gruntfile.js
+++ engine/Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-	concat: {
+		concat: {
 		options: {
 			// define a string to put between each file in the concatenated output
 			separator: ''
@@ -26,12 +26,25 @@ module.exports = function(grunt) {
 		watch: {
 		  files: ['<%= concat.dist.src %>'],
 			tasks: ['concat:dist']
-		}
+		},
+		jasmine: {
+		  src : 'src/**/*.js',
+			test:{										 
+		  	specs : 'specs/**/*spec.js',
+		  	helpers : 'specs/helpers/*.js',
+			  timeout : 10000,
+  			junit : {
+	    		output : 'junit/'
+			  	}
+	 		}
+		}	
 	});
 
+grunt.loadNpmTasks('grunt-contrib-jasmine');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-concat');
 
+grunt.registerTask('test', ['jasmine']);
 grunt.registerTask('default', ['watch']);
 
 };
