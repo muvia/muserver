190acefd4a8b05ce7d5150e5021a7d7a38c22e9e
cesarpachon <cesarpachon@gmail.com>
2014-02-25 23:02:42 -0500

    added test grid.html

--- engine/dist/muengine.js
+++ engine/dist/muengine.js
@@ -311,16 +311,24 @@ MuEngine.Node.prototype.addChild = function(node){
 	 * @param height num of columns
 	 * @param cellsize length of a cell side 
 	 */
-	MuEngine.Grid = function(width, height, cellsize){
+	MuEngine.Grid = function(width, height, cellsize, color){
 		this.width = width;
 		this.height = height;
 	 	this.cellsize = cellsize;
+		this.color = color || "#cccccc"; 
 		this.cells = new Array(width * height);
 		for(var i=0; i<this.width; ++i){
 				for(var j=0; j< this.height; ++j)
 					this.cells[(i*this.width)+j] = new Cell(i, j);
 		};
 	};
+
+	/*
+	* Grid static attributes
+	*/
+	MuEngine.Grid.prototype.g_p0 = vec3.create();
+  MuEngine.Grid.prototype.g_p1 = vec3.create();
+
 
 	/**
 	 * return a Cell object for the given i,j coordinate.
@@ -334,18 +342,34 @@ MuEngine.Node.prototype.addChild = function(node){
 
 	MuEngine.Grid.prototype.render = function(mat, cam){
 		var w = this.width*this.cellsize;
-		pt[1] = 0;
-		pt[2] = 0;
-		pt2[1] = w;
-		pt2[2] = w; 		
-		//draw columns
+		this.g_p0[1] = 0;
+		this.g_p0[2] = 0;
+		this.g_p1[1] = 0;
+		this.g_p1[2] = w; 		
+		//draw rows
 		var aux = 0;
-		for(var i=0; i<=this.width; ++i){
-			pt[0] = aux;
-			pt2[0] = aux; 
+		for(var i=0; i<=this.height; ++i){
+			this.g_p0[0] = aux;
+			this.g_p1[0] = aux; 
+			vec3.transformMat4(this.g_p0, this.g_p0, mat); 
+			vec3.transformMat4(this.g_p1, this.g_p1, mat); 
+			cam.renderLine(this.g_p0, this.g_p1, this.color);
 			aux += this.cellsize;
 		};
-
+		var h = this.height*this.cellsize;
+		this.g_p0[0] = 0;
+		this.g_p0[1] = 0;
+		this.g_p1[0] = h;
+		this.g_p1[1] = 0; 		
+		aux = 0;
+		for(var j=0; j<=this.width; ++j){
+			this.g_p0[2] = aux;
+			this.g_p1[2] = aux; 
+			vec3.transformMat4(this.g_p0, this.g_p0, mat); 
+			vec3.transformMat4(this.g_p1, this.g_p1, mat); 
+			cam.renderLine(this.g_p0, this.g_p1, this.color);
+			aux += this.cellsize;
+		};
 	};
 	//------- LINE CLASS -------------------
 	
