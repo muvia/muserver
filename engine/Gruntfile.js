71edf2112b9cacc2aa36b017fdfb72d8dc5f4aa5
cesarpachon <cesarpachon@gmail.com>
2014-05-02 08:11:32 -0500

    adding jsdoc support to engine

--- engine/Gruntfile.js
+++ engine/Gruntfile.js
@@ -42,14 +42,24 @@ module.exports = function(grunt) {
 			  	timeout : 10000
 				}
 	 		}
-		}	
-	});
+		},
+ jsdoc: {
+            dist : {
+                src: ['src/*.js'],
+                options: {
+                    destination: 'jsdoc'
+                }
+            }	
+	}
+});
 
 grunt.loadNpmTasks('grunt-contrib-jasmine');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-jsdoc');
 
 grunt.registerTask('test', ['jasmine']);
 grunt.registerTask('default', ['watch']);
 grunt.registerTask('make', ['concat:dist']);
+grunt.registerTask('doc', ['jsdoc:dist']);
 };
