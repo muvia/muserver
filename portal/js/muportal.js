42f5c9db9058893217327af6ad5e0150d2abe0ab
cesarpachon <cesarpachon@gmail.com>
2014-05-07 17:30:00 -0500

    adding partialspath based on locale to main controller

--- portal/js/muportal.js
+++ portal/js/muportal.js
@@ -232,8 +232,12 @@ var muPortalApp = angular.module('muPortal', ['ngRoute', 'localization']).
  */
 
 muPortalApp.controller('mainController',
-    function($scope) {
+    function($scope, $locale) {
+
         var someText = {};
         someText.message = 'You have started your journey.';
         $scope.someText = someText;
+
+        //$locale.id has the form en-us.. we are only interested in language "en" or "es"..
+        $scope.partialspath = $locale.id.substr(0, 2) + "/";
     });
--- portal/src/controllers/main.js
+++ portal/src/controllers/main.js
@@ -4,8 +4,12 @@
  */
 
 muPortalApp.controller('mainController',
-    function($scope) {
+    function($scope, $locale) {
+
         var someText = {};
         someText.message = 'You have started your journey.';
         $scope.someText = someText;
+
+        //$locale.id has the form en-us.. we are only interested in language "en" or "es"..
+        $scope.partialspath = $locale.id.substr(0, 2) + "/";
     });
