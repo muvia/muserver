170741734b04553f8efcfb823966a131c04157f6
cesarpachon <cesarpachon@gmail.com>
2014-05-08 09:07:11 -0500

    completed support for localized partials

---
+++ portal/i18n/resources-locale_en-US.js
@@ -0,0 +1,17 @@
+[
+    {
+        "key":"_Greeting_",
+        "value":"Hello there!",
+        "description":"Home page greeting text"
+    },
+    {
+        "key":"_HomeTitle_",
+        "value":"a title",
+        "description":"Home page title text"
+    },
+    {
+        "key":"_welcome_",
+        "value":"you are welcome!",
+        "description":"Home page title text"
+    }
+]
--- portal/js/muportal.js
+++ portal/js/muportal.js
@@ -238,6 +238,13 @@ muPortalApp.controller('mainController',
         someText.message = 'You have started your journey.';
         $scope.someText = someText;
 
-        //$locale.id has the form en-us.. we are only interested in language "en" or "es"..
-        $scope.partialspath = $locale.id.substr(0, 2) + "/";
+        /**
+         * given a partialname, like "welcome_intro.html", return the relative
+         * localized path, like "partials/en/welcome_intro.html"
+         * @param partialname
+         */
+        $scope.getLocalizedPartial= function(partialname){
+            return "partials/"+$locale.id.substr(0, 2) + "/" + partialname;
+        }
+
     });
