57590a5139573e8a4c2b498ca2994de4314e2288
cesarpachon <cesarpachon@gmail.com>
2014-06-24 16:52:59 -0500

    adding contact services

--- portal/Gruntfile.js
+++ portal/Gruntfile.js
@@ -13,6 +13,7 @@ module.exports = function (grunt) {
                     'lib/localize.js',
                     'src/muportalapp.js',
                     'src/services/authsrv.js',
+                    'src/services/contactsrv.js',
                     'src/directives/authdir.js',
                     'src/controllers/mainctrl.js',
                     'src/controllers/authctrl.js',
--- portal/src/controllers/contactctrl.js
+++ portal/src/controllers/contactctrl.js
@@ -4,15 +4,26 @@
  * controller for contact form
  */
 
-muPortalApp.controller('contactController', [function() {
+muPortalApp.controller('contactController', ['contactsrv', function(contactsrv) {
 
     this.name = null;
     this.email = null;
-
     this.message = null;
 
+    this.error = null;
+
     this.doContact = function(){
-        console.log("contactController.doContact ", this.name, this.email, this.message);
+        var self = this;
+        //console.log("contactController.doContact ", this.name, this.email, this.message);
+        contactsrv.sendMessage(this.name, this.email, this.message, function(error){
+            self.error = error;
+            if(self.error === null){
+                console.log("contact message sent! how to notify the user?");
+            }
+            else{
+                console.log("contact message failed! self.error had been set.");
+            }
+        });
     }
 
 }]);
