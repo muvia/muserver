729b702dae07581476ea79a2124547bc2ebc158c
cesarpachon <cesarpachon@gmail.com>
2014-09-07 23:37:29 -0500

    profile page: wip: adding directive for accesible checkbox

---
+++ portal/src/directives/checkboxdir.js
@@ -0,0 +1,26 @@
+/**
+params: id
+the id is used for:
+labelledby: {{id}}_label
+describedby: {{id}}_desc
+i18n: _{{id}}_
+i18n: _{{id}}_desc_
+*/
+muPortalApp.directive("muCheckbox", function () {
+  'use strict';
+  return {
+    restrict: 'E',
+    replace: true,
+    link: function(scope, element, attrs) {
+      scope.id = attrs['id'];
+    },
+    template: '<div class="form-group">'+
+    '<label id="{{id}}_label"  class="col-sm-2 control-label" for="{{id}}" data-i18n="_{{id}}_"></label>'+
+        '<div class="col-sm-10">'+
+            '<input id="{{id}}" type="checkbox" value="" aria-labelledby="{{id}}_label" aria-describedby="{{id}}_desc" >'+
+            '<span id="{{id}}_desc" data-i18n="_{{id}}_desc_" ></span>'+
+        '</div>'+
+    '</div>'
+
+  };
+});
